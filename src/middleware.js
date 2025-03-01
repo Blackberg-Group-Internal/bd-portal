9ca3e6c0fd8274207858d75428f3a4d46296a718
rb-blackberg <ross.burmeister@blackberggroup.com>
2025-03-01 10:50:31 -0800

    feat: update middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,35 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
+
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/dam', request.url));
   }
-  
+
+  if (request.nextUrl.pathname.startsWith('/api/')) {
+
+    if (request.method === 'OPTIONS') {
+      return new NextResponse(null, {
+        status: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',    
+          'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+      });
+    }
+
+    const response = NextResponse.next();
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    return response;
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/', 
+  matcher: [
+    '/',
+    '/api/:path*',
+  ],
 };
